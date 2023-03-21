27eb4900225309057c485fdc7450b3d558f6f959
Yash <kandepu.yashwanth30@gmail.com>
2023-03-21 14:42:52 +0530

    main_page

--- lib/pages/navpages/main_page.dart
+++ lib/pages/navpages/main_page.dart
@@ -1,1 +1,14 @@
+import 'package:flutter/cupertino.dart';
 
+class MainPage extends StatelessWidget {
+  const MainPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text('Main Page'),
+      ),
+    );
+  }
+}
