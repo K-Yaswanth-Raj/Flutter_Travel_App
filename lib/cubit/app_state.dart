871fd11964933750d366a5ecd889355ac93b6cb0
Yash <kandepu.yashwanth30@gmail.com>
2023-03-26 09:39:09 +0530

    creating a data service folder

--- lib/cubit/app_state.dart
+++ lib/cubit/app_state.dart
@@ -2,9 +2,8 @@ import 'package:bloc/bloc.dart';
 
 import 'app_cubit.dart';
 
-class AppCubit extends Cubit<CubitStates>{
-  AppCubit() : super(InitialState()){
+class AppCubit extends Cubit<CubitStates> {
+  AppCubit() : super(InitialState()) {
     emit(WelcomeState());
   }
-
 }
